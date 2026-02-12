88e4031511b8af3603f1bb9a71cbc18cb6701288
T2 <itzjay4kYT@gmail.com>
2026-02-12 08:09:41 -0500

    Update hero.tsx

--- components/icloud/hero.tsx
+++ components/icloud/hero.tsx
@@ -380,7 +380,7 @@ export function Hero() {
 
       {/* Game! */}
       <a 
-  href="/index.html"
+  href="/games.html"
   className="px-8 py-3 bg-white text-black font-semibold rounded-full hover:bg-opacity-90 transition-all shadow-lg"
   style={{ cursor: 'pointer', fontSize: '1.1rem', textDecoration: 'none' }}
 >
