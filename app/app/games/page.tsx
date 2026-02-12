a0fbbc2ed719a62a98a8e02be2b0f4c77153b394
T2 <itzjay4kYT@gmail.com>
2026-02-12 01:14:27 -0500

    Update page.tsx

--- app/app/games/page.tsx
+++ app/app/games/page.tsx
@@ -1,1 +1,18 @@
+tsx
+export default function GamesPage() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      {/* This block lets you use your old HTML inside the new site */}
+      <div dangerouslySetInnerHTML={{ __html: `
+        
+        <!-- PASTE YOUR ENTIRE index.html CODE STARTING BELOW THIS LINE -->
 
+        <h1 style="color: white; text-align: center;">My Games Library</h1>
+        <p style="color: white; text-align: center;">Games will load here...</p>
+
+        <!-- PASTE YOUR CODE ABOVE THIS LINE -->
+
+      ` }} />
+    </div>
+  );
+}
